5d21c073753d289c6828b2e1b3b4ed60b30cd1f0
Johnny, Jia <johnny.jia@cgi.com>
2025-07-08 15:24:49 -0300

    Add login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        component: Login
+        // path: 'login',
+        // loadComponent: () => import('./login/login').then(m => m.Login)
+    }
+];
