24a5fc36dd771b5cc8fcc3c6aeeda2a55aa8cfce
Johnny, Jia <johnny.jia@cgi.com>
2025-08-29 11:37:47 -0300

    Add dashboard components

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-dashboard.component',
@@ -6,6 +7,21 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  private readonly authService = inject(AuthService);
 
+  // Data from service
+  public readonly user = this.authService.currentUser;
+
+  ngOnInit(): void {
+    this.loadDashboardData();
+  }
+
+  /**
+   * Load all dashboard data
+   */
+  loadDashboardData() {
+    const currentUser = this.authService.currentUser();
+    console.info(currentUser);
+  }
 }
